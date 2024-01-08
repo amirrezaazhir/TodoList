3de558019ef74ecdc86c981b3efa47708d5cf7ea
amirrezaazhir <amirrezawa@gmail.com>
2024-01-08 17:06:03 +0300

    new test

--- app/components/TopicsList.jsx
+++ app/components/TopicsList.jsx
@@ -1,38 +1,38 @@
 import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
+
 const getTopics = async () => {
   try {
-    const res =  await fetch("http://localhost:3000/api/topics", {
+    const res = await fetch("http://localhost:3000/api/topics", {
       cache: "no-store",
     });
 
-    if(!res.ok){
-      throw new Error("failed to fetch topics");
-
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
     }
 
     return res.json();
   } catch (error) {
-
-    console.log("error loading topics",error);
+    console.log("Error loading topics: ", error);
   }
 };
 
 export default async function TopicsList() {
-
- const {topics} = await getTopics ();
+  const { topics } = await getTopics();
 
   return (
-    
     <>
+      {topics.map((t) => (
+        <div
+          key={t._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
+          </div>
 
-    {topics.map((t) => (
-      <div key={t._id} className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
-        
-        <div>
-          <h2 className="font-bold text-2xl">{t.title}</h2>
-          <div>{t.description}</div>
           <div className="flex gap-2">
             <RemoveBtn id={t._id} />
             <Link href={`/editTopic/${t._id}`}>
@@ -40,7 +40,6 @@ export default async function TopicsList() {
             </Link>
           </div>
         </div>
-      </div>
       ))}
     </>
   );
